34a4d383d71bdb5e0d50123d5d73b19e1bf95c30
Sergei Klimovich <sergei.kl@firebirdtours.com>
2024-04-14 16:24:43 +0300

    Create project

---
+++ src/main/java/com/klimovich/qafordevs/QaForDevsApplication.java
@@ -0,0 +1,13 @@
+package com.klimovich.qafordevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaForDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaForDevsApplication.class, args);
+	}
+
+}
